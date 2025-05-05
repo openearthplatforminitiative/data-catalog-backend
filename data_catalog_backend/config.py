26946e2a905f91755c1440fc655ffe847aeb2c5e
Sander Krøglid <sander.kroglid@knowit.no>
2025-05-05 16:03:30 +0200

    fix: update uvicorn port from 8080 to 8000

--- data_catalog_backend/config.py
+++ data_catalog_backend/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     version: str = "0.0.1"
-    uvicorn_port: int = 8080
+    uvicorn_port: int = 8000
     uvicorn_host: str = "0.0.0.0"
     uvicorn_reload: bool = True
     uvicorn_proxy_headers: bool = False
