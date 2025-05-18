510925640df443081c5ab5768c13249e3d61c23c
Marie Hessen <marie.hessen@knowit.no>
2025-05-18 21:09:38 +0200

    feat: added update functionality to resources

--- data_catalog_backend/routes/v1/resource_routes.py
+++ data_catalog_backend/routes/v1/resource_routes.py
@@ -133,7 +133,6 @@ async def update_resource(
 ) -> ResourceResponse:
     resource = service.get_resource(resource_id)
     if not resource:
-        print("Resource not found hello hello")
         raise HTTPException(status_code=404, detail="Resource not found")
 
     try:
